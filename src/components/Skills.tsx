836d9e1ff5d99475282c2b1b42dc7820d6c8bd26
Piyush <bankhedepiyush25@gmail.com>
2025-07-21 16:27:54 +0530

    third  commit

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -15,9 +15,7 @@ const Skills: React.FC = () => {
     { name: 'Shell Script', icon: Code, level: 85 },
     { name: 'Prometheus', icon: Monitor, level: 70 },
     { name: 'Grafana', icon: Monitor, level: 75 },
-    { name: 'Argocd', icon: Monitor, level: 80 },
-    { name: 'Trivy', icon: Monitor, level: 90 },
-    { name: 'Owasp', icon: Monitor, level: 75 }
+    
     ,
     
   ];
